ffc6ec8ab9230ac0bcb87758badd9baf9d92b517
NIJAAHNANDH <nijavetri@gmail.com>
2016-06-24 11:03:01 +0530

    Create vowrcons.cpp

---
+++ vowrcons.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	char a;
+    	cin>>a;
+    	if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U' )
+    		cout<<"Your character is vowel";
+    	else
+    		cout<<"Your character is consonant";
+    	return 0;
+    }
