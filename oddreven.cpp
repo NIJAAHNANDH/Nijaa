23036a3311d035ec1767ce972e9ee989e91397e8
NIJAAHNANDH <nijavetri@gmail.com>
2016-06-24 10:55:56 +0530

    Create oddreven.cpp

---
+++ oddreven.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int n;
+    	cin>>n;
+    	if(n%2==0)
+    		cout<<"You have entered an even number";
+    	else
+    		cout<<"You have entered odd number";
+     
+    	return 0;
+    }
